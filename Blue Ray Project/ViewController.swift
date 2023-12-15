4b183ec75be2f1e07ac116dab9a9ea495447c705
mgsulaiman <info@mgsulaiman.com>
2023-12-15 15:11:15 +0300

    Initial Commit

---
+++ Blue Ray Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blue Ray Project
+//
+//  Created by mgsulaimanCom on 15/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
